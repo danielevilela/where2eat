b23ceaa3e391c6955513156f9ca4fbfe81f6af8f
DANIELE APARECIDA VILELA SOUZA <dvilela@br.ibm.com>
2018-06-24 14:54:19 -0300

    Signed-off-by: DANIELE APARECIDA VILELA SOUZA <danielevilela@outlook.com>

---
+++ src/main/java/com/dvilela/challenge/model/Restaurant.java
@@ -0,0 +1,15 @@
+/*package com.dvilela.challenge.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private Location location;
+
+}*/
--- src/main/java/com/dvilela/challenge/util/Util.java
+++ src/main/java/com/dvilela/challenge/util/Util.java
@@ -3,9 +3,17 @@ package com.dvilela.challenge.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dvilela.challenge.model.Location;
 
+
 public class Util {
+	
+	 private String googleKey = "AIzaSyBa8mPI3jgw6L5YPfhkXTyaZmzsTJEwwWc";
+	 private static final Logger logger = LoggerFactory.getLogger(Util.class);
+
 
 	public int distanceCalculate(int x1,int y1, int x2, int y2) {		
 		int manhattanDistance = Math.abs(x1-x2) + Math.abs(y1-y2);
@@ -18,6 +26,7 @@ public class Util {
 		int distance = 0;
 		for(Location l : allLocations) {
 			distance = distanceCalculate(userLocation.getValueX(),userLocation.getValueY(),l.getValueX(),l.getValueY());
+			logger.info("x => "+l.getValueX()+" y=>"+l.getValueY());
 			distances.add(distance);
 		}
 		
